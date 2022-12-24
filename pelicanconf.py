2d3f2c598a4a8d9e69f66df94aa63b5ced360efe
Sean Anderson <seanga2@gmail.com>
2022-12-24 00:12:33 -0500

    Add wavedrom support
    This adds support for wavedrom timing diagrams.

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,6 +5,7 @@ TIMEZONE = 'America/New_York'
 DEFAULT_LANG = 'en'
 
 THEME = 'blue-penguin'
+THEME_TEMPLATES_OVERRIDES = ('templates',)
 
 PLUGIN_PATHS = ('pelican-plugins',)
 PLUGINS = ('asciidoc_reader', 'filetime_from_git')
