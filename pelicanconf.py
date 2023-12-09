56373e89cc6492125df86d7923bdf205ce203281
Sean Anderson <seanga2@gmail.com>
2023-12-09 13:36:38 -0500

    Support syntax highlighting
    Add support for syntax highlighting with rouge. The theme was generated
    like
    
    	pygmentize -S abap -f html -a .highlight > content/theme/css/pygments.css
    
    Adjust the paragraph vertical margins to be on both the top and bottom,
    which looks better with code blocks in the mix.

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,9 +6,13 @@ DEFAULT_LANG = 'en'
 
 THEME = 'blue-penguin'
 THEME_TEMPLATES_OVERRIDES = ('templates',)
+STATIC_PATHS = ('theme',)
 
 PLUGIN_PATHS = ('pelican-plugins',)
 PLUGINS = ('asciidoc_reader', 'filetime_from_git')
 ASCIIDOC_CMD = 'asciidoctor'
+ASCIIDOC_OPTIONS = [
+    '-a', 'source-highlighter=rouge',
+]
 
 DELETE_OUTPUT_DIRECTORY = True
